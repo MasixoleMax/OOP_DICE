75ad4ac700c4e4d67d4b33b57cf0eed28a6e79ea
MasixoleMax <masixole.menqe@umuzi.org>
2019-10-30 14:22:30 +0200

    added spec and src files

---
+++ spec/diceSpec.js
@@ -0,0 +1,9 @@
+
+describe("A dice that can have as many sides and also have the option of taking probabilities", function(){
+    it ("should pop out a random side out of six sides with different probabailities", function() {
+         var diceTest = die6.roll();
+        if(diceTest > 0 && diceTest < 8){ diceTest = true};
+        expect(diceTest).toBe(true);
+
+    });
+});
