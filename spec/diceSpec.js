35e7b4228ecfefa7209a6f6df87bacb1a18ad247
MasixoleMax <masixole.menqe@umuzi.org>
2019-12-11 16:53:21 +0200

    an updated version of the dice after revision

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -1,9 +1,46 @@
+let Dice = require('../src/diceSrc');
+let die6 = new Dice(6, [1,1,1,1,1,6]);
 
 describe("A dice that can have as many sides and also have the option of taking probabilities", function(){
-    it ("should pop out a random side out of six sides with different probabailities", function() {
-         var diceTest = die6.roll();
-        if(diceTest > 0 && diceTest < 8){ diceTest = true};
-        expect(diceTest).toBe(true);
 
+    it ("should pop out a random side out of six sides with different probabailities", function() {
+
+        var diceTest = die6.roll();
+
+        if(diceTest > 0 && diceTest < 7){ 
+            diceTest = true;
+        };
+
+        expect(diceTest).toBe(true);
+});
+
+    it ("should throw an error when the amount of set probabilities are not equal to the number of sides", function() {
+
+        expect(()=>{die6.setProbabilities([1,1,1,1,6])}).toThrow(new Error("Probabilities are not equal to the sides!"))
     });
 });
+
+    it ("should throw an error when one of the probabilities is equal to zero", function() {
+
+        expect(()=>{die6.setProbabilities([1,1,1,0,1,6])}).toThrow(new Error("A probability can not be equal to zero!"))
+});
+
+    it ("the sum of the probabilities array should be greater than 1", function() {
+
+        expect(()=>{die6.setProbabilities([0.1,0.2,0.1,0.2,0.1,0.1])}).toThrow(new Error("probability sum must be greater than 0"))
+});
+
+    it ("the sum of the probabilities array should be greater than 1", function() {
+
+        expect(()=>{die6.setProbabilities([1,1,1,1,-2,1])}).toThrow(new Error("A probability can not be a negative value!"))
+});
+
+
+
+
+
+
+
+
+
+
--- src/diceSrc.js
+++ src/diceSrc.js
@@ -1,55 +1,66 @@
-class Dice {
+ class Dice {
 
-    constructor (sides, probabilities){
-            this.numberOfSides = sides;
+    constructor (sides, probabilities){ 
+            this.sides = sides;
+            this.value = 0;
             this.sidesArr(sides);
             if(arguments.length = 2){
-                this.probabilities = probabilities;
-            };
-            this.setProbabilities(this.probabilities);
-            
-}
+                this.probabilities = probabilities
+        }
+            this.setProbabilities(this.probabilities);          
+    }
+
+// setting probabilities:
 
     setProbabilities(probabilities){
         this.probabilities = probabilities;
+        const digit = new RegExp(/\d+/);
             for (let i = 0; i < probabilities.length; i++){
+                var sum = 0
+                    sum += probabilities[i];
                 while (probabilities.length !== this.allSideValues.length){
                     throw new Error ("Probabilities are not equal to the sides!");
 
-       }    if (probabilities[i] == 0) {
-                    throw new Error ("A probability can not equal zero!");    
-       }      
-            if (probabilities[i] < 0) {
+    }   if (probabilities[i] == 0) {
+                    throw new Error ("A probability can not be equal to zero!");   
+            }      
+        if (probabilities[i] < 0) {
                     throw new Error ("A probability can not be a negative value!");
+            }   
+        if (sum < 1) {
+                    throw new Error ("probability sum must be greater than 0");
+            }
+        if (!typeof probabilities[i] === "number") {
+            throw new Error ("only integer values allowed");
         }
 
-            if (probabilities[i] > this.numberOfSides) {
-                    throw new Error ("Sorry, no probability for this dice can be greater than " + this.numberOfSides + " !");
-}
-       
+        } 
     } 
-} 
+
+// a method to create an array of sides 
     
     sidesArr(sides) {   
-        var sides = this.numberOfSides
-            this.allSideValues = [];
+        var sides = this.sides
+            this.allSideValues = []
             Number.prototype.to = function* (end) {
                 const start = this;
-                const step = end > start ? 1 : -1;
+                const step = end > start ? 1 : -1
                 const fn = function* (n) {
-                    let next = start + step * n;
-                    yield next;
-                    if (next === end) return;
-                    yield* fn(n + 1);
+                    let next = start + step * n
+                    yield next
+                    if (next === end) return
+                    yield* fn(n + 1)
                 }
-            yield start;
-            yield* fn(1);   
+            yield start
+            yield* fn(1)   
             }
         const arrB = [...(0).to(sides)];
             for(let i = 1; i < arrB.length; i++){
-                this.allSideValues.push(arrB[i]);        
-            }          return this.allSideValues;
+                this.allSideValues.push(arrB[i])      
+            }         return this.allSideValues
     }            
+
+// the rolling method
 
     roll(){
 
@@ -57,23 +68,66 @@ class Dice {
 
         for (let i = 0; i < this.probabilities.length; i++){
                 var multiplies = this.probabilities[i];
-            for (let j = 0; j < multiplies; j++){ 
-                this.weighedSides.push(this.allSideValues[i]);
-            }
-            var value = [];
-            value =  Math.floor((Math.random()* this.weighedSides.length) + 1);
-            this.randomIndexOfWeighedSides = [];           
-            this.randomIndexOfWeighedSides.push(value);
-            return this.randomIndexOfWeighedSides;
-                                             
+
+                for (let j = 0; j < multiplies; j++){ 
+                    this.weighedSides.push(this.allSideValues[i]);
+                }
+
+            let value = [];
+            var value2 = [];
+
+                value = this.weighedSides[Math.floor(Math.random() * this.weighedSides.length)];
+                value2 = this.allSideValues[Math.floor(Math.random() * this.allSideValues.length)];
+
+                this.randomValueOfWeighedSides = [];  
+                this.randomValueOfWeighedSides.push(value);          
         } 
-    }   
+            return this.value += value2;
+    }
+}   
+
+// instantiating the dice object(s)
+
+let die6 =  new Dice(6, [1,1,1,1,1,3]);
+let die10 = new Dice(10, [1,1,1,1,1,1,1,1,1,6])
+
+// the DiceFactory 
+
+class DiceFactory {
+    constructor (dieSides, probabilities){
+        this.dieSides = dieSides;
+        this.probabilities = probabilities;
+    }
+
+ // the makeDie method  
+
+    makeDie(){
+        return new DiceFactory(this.dieSides, this.probabilities)
+    }
 }
 
-            /*********************************[END]********************************/
+// instantiating the diceFcatory object(s)
+
+let factory6 = new DiceFactory(6, [1,1,1,1,1,1]); 
+let dice6 = new DiceFactory(6, [1,1,1,1,1,1]).makeDie()
+
+
+
+         /*********************************[END]********************************/
+
+module.exports = 
+                Dice,
+                DiceFactory; 
             
+die6.roll();
+console.log("----------------------------------------")
+console.log(die6.value)
+console.log("----------------------------------------")
+console.log(die6)
+console.log("----------------------------------------")
 
 
-    let die6 = new Dice(6, [1,1,1,1,1,2]);
-    die6.roll();
-    console.log(die6);
+console.log(factory6);
+console.log(dice6);
+
+
